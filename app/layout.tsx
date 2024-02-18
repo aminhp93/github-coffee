5418930f2455513cabe4f31306f749955bc40453
aminhp93 <minhpn.org.ec@gmail.com>
2024-02-18 12:13:54 +0700

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AppBar from "./AppBar";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AppRouterCacheProvider>
-          <AppBar>{children}</AppBar>
+          <AppBar>
+            {children}
+            <Analytics />
+          </AppBar>
         </AppRouterCacheProvider>
       </body>
     </html>
