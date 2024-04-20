baa433906cf5be3803467999ec80a0b38227c18f
aminhp93 <minhpn.org.ec@gmail.com>
2024-04-20 19:38:47 +0700

    Update

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -3,7 +3,11 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 
 const DetailBlog = async (props: any) => {
   const id = props.params.id;
-  const file = await fs.readFile(process.cwd() + `/content/${id}`, "utf8");
+  const file = await fs.readFile(
+    process.cwd() + `/public/content/${id}`,
+    "utf8"
+  );
+  // seem like it does not work. get error path: '/var/task/content/github-coffee-problem-1.mdx'
 
   return (
     <div>
