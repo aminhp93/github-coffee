cd446d7bfcdc2dc21bf5ae9bff3f3f711140d346
Minh Pham <minhpham@Minhs-Mac-mini.localdomain>
2025-02-21 16:43:08 +0700

    add base new structure with nextjs, mui and tailwind (#15)

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { StyledEngineProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return(
+    <StyledEngineProvider injectFirst>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </StyledEngineProvider>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <Box className="text-3xl font-bold underline">
+    Box
+  </Box>
+    </div>
+  );
+}
