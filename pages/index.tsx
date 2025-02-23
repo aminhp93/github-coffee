b8659e79d1d8c79ee94fae6878e8a9bcb4cb0a1b
aminhp93 <minhpn.org.ec@gmail.com>
2025-02-23 19:29:06 +0700

    add check npm package page (#15)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import { Box } from "@mui/material";
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <Box className="text-3xl font-bold underline">
-    Box
-  </Box>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Box className="text-3xl font-bold underline">Box</Box>
+      <Link href="/check-npm-package">Check npm package</Link>
     </div>
   );
 }
