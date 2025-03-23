b7714d1c8c6ece753051da13454f15d36fa3f72c
aminhp93 <minhpn.org.ec@gmail.com>
2025-03-23 10:34:17 +0700

    Add description feature as markdown (#18)

---
+++ @core/pages/feature-wrapper/index.tsx
@@ -0,0 +1,49 @@
+import { ReactNode, useEffect, useState } from "react";
+import { Button, Drawer } from "@mui/material";
+import matter from "gray-matter";
+import Markdown from "react-markdown";
+
+const FeatureWrapper = ({
+  children,
+  featureName,
+}: {
+  children: ReactNode;
+  featureName: string;
+}) => {
+  const [open, setOpen] = useState(false);
+  const [content, setContent] = useState("");
+
+  const [meta, setMeta] = useState<{
+    title?: string;
+    description?: string;
+    link?: string;
+  }>({});
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  useEffect(() => {
+    fetch(`/docs/features/${featureName}.md`)
+      .then((res) => res.text())
+      .then((raw) => {
+        const { data, content: mdContent } = matter(raw);
+        setMeta(data);
+        setContent(mdContent);
+      });
+  }, [featureName]);
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer(true)}>Feature detail</Button>
+      {children}
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        <h1>{meta.title}</h1>
+        <p>{meta.description}</p>
+        <Markdown>{content}</Markdown>
+      </Drawer>
+    </div>
+  );
+};
+
+export default FeatureWrapper;
--- pages/check-npm-package/index.tsx
+++ pages/check-npm-package/index.tsx
@@ -4,6 +4,14 @@ const CheckNpmPackage = dynamic(() => import("@/features/check-npm-package"), {
   ssr: false,
 });
 
+const FeatureWrapper = dynamic(() => import("@/@core/pages/feature-wrapper"), {
+  ssr: false,
+});
+
 export default function CheckNpmPackagePage() {
-  return <CheckNpmPackage />;
+  return (
+    <FeatureWrapper featureName="check-npm-package">
+      <CheckNpmPackage />;
+    </FeatureWrapper>
+  );
 }
