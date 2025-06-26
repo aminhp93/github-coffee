bf40d97d200c88b5e18941cd81b05495138e4cc5
Minh Pham <minhpham@Minhs-Mac-mini.localdomain>
2025-06-26 11:02:31 +0700

    add workspace (#33)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { NavLink, Link } from 'react-router';
 
+import { sum } from 'sum-utils';
+
 function App() {
+  const result = sum(5, 3);
+  console.log(`The sum is: ${result}`);
   return (
     <>
       <nav>
