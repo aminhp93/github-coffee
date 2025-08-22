5f34d666a0fa5f7bfd9d0ef7026a055d6e58cc1b
aminhp93 <minhpn.org.ec@gmail.com>
2025-08-22 10:40:21 +0700

    Add Copilot instructions and coding standards for the project

--- packages/sum-utils/src/index.ts
+++ packages/sum-utils/src/index.ts
@@ -1,3 +1,10 @@
 export function sum(a: number, b: number): number {
   return a + b;
 }
+
+export function test() {
+  const xxx = "123";
+  const yyy = "456";
+  const zzz = sum(parseInt(xxx), parseInt(yyy));
+  return zzz;
+}
